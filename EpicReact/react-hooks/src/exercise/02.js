bcbc96280cbfa2daa1a65a19caf2676e34009f6c
Ana Karina Morales Machado <akarinammachado@gmail.com>
2021-05-25 00:54:04 +0200

    🎣 (Epic React - React Hooks) Exercise 2: extra creditos 1 and 2

--- EpicReact/react-hooks/src/exercise/02.js
+++ EpicReact/react-hooks/src/exercise/02.js
@@ -3,12 +3,36 @@
 
 import * as React from 'react'
 
+// Exercice 1
+// function Greeting({initialName = ''}) {
+//   const [name, setName] = React.useState(window.localStorage.getItem('name') || initialName)
+
+//   console.log(name)
+
+//   React.useEffect(() => window.localStorage.setItem('name', name))
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// 💯 Extra credit 1: lazy state initialization
+// 💯 Extra credit 2: effect dependencies
 function Greeting({initialName = ''}) {
-  const [name, setName] = React.useState(window.localStorage.getItem('name') || initialName)
+  const getInitialState = () => window.localStorage.getItem('name') || initialName
+  const [name, setName] = React.useState(getInitialState)
 
-  console.log(name)
-
-  React.useEffect(() => window.localStorage.setItem('name', name))
+  React.useEffect(() => window.localStorage.setItem('name', name), [name])
 
   function handleChange(event) {
     setName(event.target.value)
