328011bb22cb332424532bcfc9faf981baa29518
Ana Karina Morales Machado <akarinammachado@gmail.com>
2021-05-30 19:35:55 +0200

    🎣 (Epic React - React Hooks) Exercise 2: extra credit 3 - custom hook

--- EpicReact/react-hooks/src/exercise/02.js
+++ EpicReact/react-hooks/src/exercise/02.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-// Exercice 1
+// Exercise 1
 // function Greeting({initialName = ''}) {
 //   const [name, setName] = React.useState(window.localStorage.getItem('name') || initialName)
 
@@ -28,14 +28,44 @@ import * as React from 'react'
 
 // 💯 Extra credit 1: lazy state initialization
 // 💯 Extra credit 2: effect dependencies
+// function Greeting({initialName = ''}) {
+//   const getInitialState = () => window.localStorage.getItem('name') || initialName
+//   const [name, setName] = React.useState(getInitialState)
+
+//   React.useEffect(() => window.localStorage.setItem('name', name), [name])
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// 💯 Extra credit 3: create custom hook
+function useLocalStorageState(key, defaultValue = '') {
+  const getInitialState = () => window.localStorage.getItem(key) || defaultValue
+  const [state, setState] = React.useState(getInitialState)
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
-  const getInitialState = () => window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(getInitialState)
+  const [name, setName] = useLocalStorageState('name', initialName)
 
-  React.useEffect(() => window.localStorage.setItem('name', name), [name])
-
-  function handleChange(event) {
-    setName(event.target.value)
+  const handleChange = evt => {
+    setName(evt.target.value)
   }
 
   return (
