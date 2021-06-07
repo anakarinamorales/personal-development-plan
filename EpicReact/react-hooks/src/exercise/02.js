489d640d4c69ae722e2adbe16bc1bac6c1bc0a69
Ana Karina Machado <a.machado@gracious.nl>
2021-06-07 11:55:50 +0200

    🎣 (Epic React - React Hooks) Exercise 2: extra credit 4 - flexible localStorage hook

--- EpicReact/react-hooks/src/exercise/02.js
+++ EpicReact/react-hooks/src/exercise/02.js
@@ -50,13 +50,67 @@ import * as React from 'react'
 // }
 
 // 💯 Extra credit 3: create custom hook
-function useLocalStorageState(key, defaultValue = '') {
-  const getInitialState = () => window.localStorage.getItem(key) || defaultValue
-  const [state, setState] = React.useState(getInitialState)
+// function useLocalStorageState(key, defaultValue = '') {
+//   const getInitialState = () => window.localStorage.getItem(key) || defaultValue
+//   const [state, setState] = React.useState(getInitialState)
 
+//   React.useEffect(() => {
+//     window.localStorage.setItem(key, state)
+//   }, [key, state])
+
+//   return [state, setState]
+// }
+
+// function Greeting({initialName = ''}) {
+//   const [name, setName] = useLocalStorageState('name', initialName)
+
+//   const handleChange = evt => {
+//     setName(evt.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// useEffect: persistent state
+// 💯 flexible localStorage hook
+function useLocalStorageState(
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      // the try/catch is here in case the localStorage value was set before
+      // we had the serialization in place (like we do in previous extra credits)
+      try {
+        return deserialize(valueInLocalStorage)
+      } catch (error) {
+        window.localStorage.removeItem(key)
+      }
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
+
+  // Check the example at src/examples/local-state-key-change.js to visualize a key change
   React.useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, state, serialize])
 
   return [state, setState]
 }
@@ -64,8 +118,8 @@ function useLocalStorageState(key, defaultValue = '') {
 function Greeting({initialName = ''}) {
   const [name, setName] = useLocalStorageState('name', initialName)
 
-  const handleChange = evt => {
-    setName(evt.target.value)
+  function handleChange(event) {
+    setName(event.target.value)
   }
 
   return (
